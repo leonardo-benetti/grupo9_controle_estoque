8cdfad2a5f2004a1661e0c95fd71ac986c2329f8
renanikeda <renan.ikeda@hotmail.com>
2022-09-20 22:43:55 -0300

    New versionWPF

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace grupo9_controle_estoque
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
