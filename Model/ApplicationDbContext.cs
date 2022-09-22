23790fed3835be04ed3f47483e61746316e56b08
renanikeda <renan.ikeda@hotmail.com>
2022-09-21 21:28:15 -0300

    Final Aula 2

---
+++ Model/ApplicationDbContext.cs
@@ -0,0 +1,62 @@
+﻿using System.IO;
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace grupo9_controle_estoque.Model
+{
+    internal class ApplicationDbContext:DbContext
+    {
+        public static void CreateFolderAndFile(string path)
+        {
+            string root_path = path.Split("/")[0];
+
+            if (!Directory.Exists(root_path))
+            {
+                Directory.CreateDirectory(root_path);
+            }
+
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+        }
+        public virtual DbSet<Users> Users { get; set; }
+        public virtual DbSet<Products> Products { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            CreateFolderAndFile("PersistentData/DB");
+            CreateFolderAndFile("PersistentData/ProfilePictures");
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string pathDB = System.IO.Path.Combine(currentDirectory, "PersistentData/DB/Data");
+            Console.WriteLine(pathDB);
+            optionsBuilder.UseSqlite($"Data Source={pathDB}");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Users>().HasData(
+            new Users
+            {
+                Name = "Administrador",
+                Pwd_salt = "1231",
+                Pwd_hash = "123312",
+                Profile_pic = ""
+            });
+
+            modelBuilder.Entity<Products>().HasData(
+            new Products
+            {
+                Name = "Livro",
+                Description = "O poder do foda-se",
+                Price = 50,
+                Quantity = 5,
+                Category = "Auto Ajuda"
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
