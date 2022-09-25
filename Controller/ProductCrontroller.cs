0b91737662fc1ace9c88bd1ad07168d60ebcf57a
renanikeda <renan.ikeda@hotmail.com>
2022-09-25 12:44:23 -0300

    change controller

---
+++ Controller/ProductCrontroller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using grupo9_controle_estoque.Model;
+
+namespace grupo9_controle_estoque.Controller;
+internal class ProductCrontroller
+{
+    private readonly ApplicationDbContext context;
+    public ProductCrontroller(ApplicationDbContext context)
+    {
+        this.context = context;
+    }
+    public List<Product> GetProducts()
+    {
+        return this.context.Products.ToList();
+    }
+}
---
+++ Controller/UserController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using grupo9_controle_estoque.Model;
+
+namespace grupo9_controle_estoque.Controller;
+internal class UserController
+{
+    private readonly ApplicationDbContext context;
+    public UserController(ApplicationDbContext context)
+    {
+        this.context = context;
+    }
+    public List<User> GetUsers()
+    {
+        return this.context.Users.ToList();
+    }
+
+}
