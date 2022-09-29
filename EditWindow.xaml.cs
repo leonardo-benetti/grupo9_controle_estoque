403536ad221dde65c7adea6f5d81c29feb85ff0c
renanikeda <renan.ikeda@hotmail.com>
2022-09-29 15:51:37 -0300

    Added save/close buttons

--- EditWindow.xaml.cs
+++ EditWindow.xaml.cs
@@ -12,14 +12,33 @@ namespace grupo9_controle_estoque;
 public partial class EditWindow : Window
 {
     private Product product;
+    private Product newProduct;
     private List<Product> productList;
     public EditWindow(Product product)
     {
         this.product = product;
+        this.newProduct = new Product() {
+            GUID = product.GUID,
+            Category = product.Category,
+            Name = product.Name,
+            Price = product.Price,
+            Quantity = product.Quantity,
+            Description = product.Description
+        };
         InitializeComponent();
         this.productList = new List<Product>();
         this.productList.Add(product);
         ProductEditGrid.ItemsSource = this.productList;
+    }
+
+    private void SaveChanges(object s, RoutedEventArgs e)
+    {
+        this.Close();
+    }
+
+    private void DiscartChanges(object s, RoutedEventArgs e)
+    {
+        this.Close();
     }
 }
 
