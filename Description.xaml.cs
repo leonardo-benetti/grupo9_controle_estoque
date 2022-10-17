099ecb154030fd8e083409adebcf785e7976ebfc
leonardo-benetti <leonardo.benetti@usp.br>
2022-10-16 21:24:40 -0300

    feat: add MahApps for styling

--- Description.xaml.cs
+++ Description.xaml.cs
@@ -6,12 +6,14 @@ using System.Threading.Tasks;
 using System.Windows;
 using grupo9_controle_estoque.Model;
 using grupo9_controle_estoque.Controller;
+using MahApps.Metro.Controls;
+
 namespace grupo9_controle_estoque
 {
     /// <summary>
     /// Interaction logic for Description.xaml
     /// </summary>
-    public partial class Description : Window
+    public partial class Description : MetroWindow
     {
         private Product product;
         public Description(Product product)
